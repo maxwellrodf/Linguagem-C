3495d03244b59cb2b3defd6f7d43d323907f8037
Maxwellrodf <63932869+Maxwellrodf@users.noreply.github.com>
2021-09-10 22:34:45 -0300

    Exercício 4 Lista 2 Com Switch Case

--- Exercicio 4 com Switch Case.cpp
+++ Exercicio 4 com Switch Case.cpp
@@ -1,4 +1,4 @@
-//EXERCICIO 4 DA LISTA 2
+//EXERCICIO 4 DA LISTA 2 COM SWITCH CASE
 //Elaborar um programa que leia 3 notas de um aluno e a média das notas dos exercícios realizados por ele. Calcular a média de aproveitamento, usando a fórmula: MA = [(N1 + N2*2 + N3*3 + ME)/7]. A partir da média, informar o conceito de acordo com a tabela
 
 #include <stdio.h>
